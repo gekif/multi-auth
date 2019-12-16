e9dd3673e16449aa34052643bfd25ca50cb31eb5
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-16 15:20:24 +0700

    Modify LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -18,7 +22,9 @@ class LoginController extends Controller
     |
     */
 
+
     use AuthenticatesUsers;
+
 
     /**
      * Where to redirect users after login.
@@ -26,6 +32,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+
 
     /**
      * Create a new controller instance.
@@ -35,5 +42,67 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->middleware('guest:writer')->except('logout');
     }
+
+
+    public function showAdminLoginForm()
+    {
+        return view('auth.login', ['url' => 'admin']);
+    }
+
+
+    public function adminLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('admin')
+            ->attempt([
+                'email' => $request->email,
+                'password' => $request->password],
+                $request->get('remember'))) {
+
+            return redirect()->intended('/admin');
+
+        }
+
+        return back()->withInput($request->only('email', 'remember'));
+
+    }
+
+
+    public function showWriterLoginForm()
+    {
+        return view('auth.login', ['url' => 'writer']);
+    }
+
+
+    public function writerLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('writer')
+            ->attempt([
+                'email' => $request->email,
+                'password' => $request->password],
+                $request->get('remember'))) {
+
+            return redirect()->intended('/writer');
+        }
+
+        return back()->withInput($request->only('email', 'remember'));
+
+    }
+
+
+
+
+
 }
