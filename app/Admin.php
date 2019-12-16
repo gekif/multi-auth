3a443e1a3f7fcb45e017bbe28bae66c19d613231
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-16 15:14:44 +0700

    Admin model

---
+++ app/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Model
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+}
